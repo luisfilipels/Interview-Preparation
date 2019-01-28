ee525e730a20c1d91234f52ab46ee223e04f420d
Luis Filipe <luisfilipels.lf@gmail.com>
2019-01-28 12:04:08 -0300

    Initial commit

---
+++ Bubble Sort/cmake-build-debug/CMakeFiles/Bubble_Sort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bubble_Sort.dir/main.cpp.obj"
+  "Bubble_Sort.pdb"
+  "Bubble_Sort.exe"
+  "Bubble_Sort.exe.manifest"
+  "libBubble_Sort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bubble_Sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Bubble Sort/cmake-build-debug/CMakeFiles/Bubble_Sort.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Bubble_Sort.
+# This may be replaced when dependencies are built.
