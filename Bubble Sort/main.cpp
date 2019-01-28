1a249e53e400bd97fe21a4149a45b077eb0876cb
luisfilipels <luisfilipels.lf@gmail.com>
2019-01-28 19:23:18 -0300

    Finalizando Bubble Sort
    Adicionados comentários e função auxiliar.

--- Bubble Sort/main.cpp
+++ Bubble Sort/main.cpp
@@ -5,23 +5,38 @@
 #include <cstdlib>
 #include <random>
 
-#define SIZE 100
-#define MAX 100
+#define SIZE 20
+#define MAX 50
 
-std::vector<int> fillRand (int size, int max) {
+std::vector<int> fillRand (int size, int max) { // Preencher vetor com números aleatórios.
     std::mt19937 mersenne(static_cast<unsigned int>(std::time(nullptr)));
     std::uniform_int_distribution<> die(1, max);
     std::vector<int> vet;
     for (int i = 0; i < size; i++) {
-        vet.push_back(die(mersenne));aaaaa
+        vet.push_back(die(mersenne));
     }
     return vet;
 }
 
+void swap (int &a, int &b) {
+    int aux;
+    aux = a;
+    a = b;
+    b = aux;
+}
+
 int main() {
     std::vector<int> vet = fillRand(SIZE, MAX);
-    for (int i = 1; i < SIZE; i++) {
-        std::cout << vet[i]  << std::endl;
+    int aux;
+    for (int i = 1; i < SIZE; i++) {    //Bubble sort, propriamente dito.
+        for (int j = 0; j < SIZE - i; j++) {
+            if (vet[j] > vet[j+1]) {
+                swap(vet[j], vet[j+1]);
+            }
+        }
+    }
+    for (int k = 0; k < SIZE; k++) {
+        std::cout << vet[k] << " ";
     }
     return 0;
 }
