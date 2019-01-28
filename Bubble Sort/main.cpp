ac8f4ac88710a2d9570931eb111b15950a536901
Luis Filipe <luisfilipels.lf@gmail.com>
2019-01-28 13:01:45 -0300

    main

--- Bubble Sort/main.cpp
+++ Bubble Sort/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <chrono>
+#include <cstdlib>
+#include <random>
+
+#define SIZE 100
+#define MAX 100
+
+std::vector<int> fillRand (int size, int max) {
+    std::mt19937 mersenne(static_cast<unsigned int>(std::time(nullptr)));
+    std::uniform_int_distribution<> die(1, max);
+    std::vector<int> vet;
+    for (int i = 0; i < size; i++) {
+        vet.push_back(die(mersenne));
+    }
+    return vet;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> vet = fillRand(SIZE, MAX);
+    for (int i = 1; i < SIZE; i++) {
+        std::cout << vet[i]  << std::endl;
+    }
     return 0;
 }
