23cc081618e13c9a34cf16691789b1c145f9ab3c
Luis Filipe <luisfilipels.lf@gmail.com>
2019-02-13 14:20:29 -0300

    Atualizado testMerge

--- SortingAlgorithms(Python)/testMerge.py
+++ SortingAlgorithms(Python)/testMerge.py
@@ -10,14 +10,11 @@ from mergeSort import *
 
 setup = "from helperFunctions import printIfSorted\n" \
         "from mergeSort import mergeSort"
-vect = geraListaReversa(500000)
+vect = geraListaReversa(50000)
 swaps = []
 tempo = []
 passo = []
-#vect = mergeSort(vect)
-#printIfSorted(vect)
 for x in range(10000, len(vect)+1, 10000):
-    #tempo.append(timeit.timeit("aux=[{0}]\naux=mergeSort.mergeSort(aux)\nprintIfSorted(aux)".format(vect[0:x]), setup, number=1))
     tempo.append(timeit.timeit("mergeSort({})".format(vect[0:x]), setup, number=1))
     passo.append(x)
     swaps.append(variables.swaps)
