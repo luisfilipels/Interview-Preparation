fa4c151915a9595a925b00b5dd635d5ebb4a0217
luisfilipels <luisfilipels.lf@gmail.com>
2019-02-19 21:00:27 -0300

    Implementado Quick Sort In Place

--- SortingAlgorithms(Python)/quickSort.py
+++ SortingAlgorithms(Python)/quickSort.py
@@ -20,6 +20,25 @@ def partition(v, n):
     variables.swaps += 1
     return j
 
+def partitionInPlace (v, n, inicio, fim):
+    v[inicio], v[n] = v[n], v[inicio]
+    pivo = v[inicio]
+    i = inicio + 1
+    j = fim
+    while i <= j:
+        while i <= j and v[i] < pivo:
+            i += 1
+        while v[j] > pivo:
+            j -= 1
+        if i <= j:
+            v[i], v[j] = v[j], v[i]
+            variables.swaps += 1
+            i += 1
+            j -= 1
+    v[j], v[inicio] = v[inicio], v[j]
+    variables.swaps += 1
+    return j
+
 def quickSort(v):
     if len(v) <= 1:
         return v
@@ -27,6 +46,14 @@ def quickSort(v):
     left = quickSort(v[:j+1])
     right = quickSort(v[j+1:])
     return left + right
+
+def quickSortInPlace(v, inicio, fim):
+    if fim - inicio <= 1:
+        return v
+    j = partitionInPlace(v, inicio, inicio, fim-1)
+    quickSortInPlace(v, inicio, j)
+    quickSortInPlace(v, j + 1, fim)
+    return v
 
 def quickSortNDet(v):
     if len(v) <= 1:
--- SortingAlgorithms(Python)/testQuick.py
+++ SortingAlgorithms(Python)/testQuick.py
@@ -1,7 +1,16 @@
 from helperFunctions import *
 import timeit
 import variables
+from quickSort import *
 
+#vect = [6, 9, 4, 7, 3, 5, 2, 0, 1, 8]
+#vect = [8, 6, 1, 5, 9, 7, 3, 2, 4, 0]
+vect = geraLista(1000)
+print(vect)
+quickSortInPlace(vect, 0, len(vect))
+printIfSorted(vect)
+print(vect)
+'''
 setup = "import helperFunctions\n" \
         "from quickSort import quickSort, quickSortNDet"
 vect = geraLista(50000)
@@ -20,3 +29,4 @@ for x in range(10000, len(vect)+1, 10000):
 
 desenhaGrafico(passo, tempo, xl="Elementos", yl="Tempo", titulo="Quicksort Nao Deterministico-Tempo")
 desenhaGrafico(passo, swaps, xl="Elementos", yl="Swaps", titulo="Quicsort Nao Deterministico-Swaps")
+'''
