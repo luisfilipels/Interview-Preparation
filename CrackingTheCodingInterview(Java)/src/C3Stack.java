699544a0788fd21418cc436cd2b49406fa4dbdf3
Luis Filipe <luisfilipels@hotmail.com>
2019-07-11 23:00:57 -0300

    Implementada pilha utilizando lista ligada.

---
+++ CrackingTheCodingInterview(Java)/src/C3Stack.java
@@ -0,0 +1,170 @@
+public interface C3Stack {
+    public class ArrayStack {
+        private int head;
+        private int [] stack;
+
+        ArrayStack(){
+            head = Integer.MIN_VALUE;
+            stack = new int[1];
+        }
+
+        public boolean empty() {
+            return head == Integer.MIN_VALUE;
+        }
+
+        public void append (int d) {
+            if (head == stack.length - 1) {
+                int [] tempStack = new int[stack.length*2];
+                for (int i = 0; i < stack.length; i++) {
+                    tempStack[i] = stack[i];
+                }
+                stack = tempStack;
+            }
+            if (head == Integer.MIN_VALUE) {
+                stack[0] = d;
+                head = 0;
+            } else {
+                head++;
+                stack[head] = d;
+            }
+        }
+
+        public void clear() {
+            head = Integer.MIN_VALUE;
+        }
+
+        public int pop() {
+            try {
+                if (!(head == Integer.MIN_VALUE)) {
+                    int returnInt = stack[head];
+                    head--;
+                    if (head == -1) {
+                        head = Integer.MIN_VALUE;
+                    }
+                    return returnInt;
+                } else {
+                    throw new ArrayIndexOutOfBoundsException();
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+                return Integer.MIN_VALUE;
+            }
+        }
+
+        @Override
+        public String toString() {
+            if (head == Integer.MIN_VALUE) {
+                return "null";
+            }
+            String returnString = "";
+            for (int i = head; i >= 0; i--) {
+                returnString += Integer.toString(stack[i]) + " ";
+            }
+            return returnString;
+        }
+
+        public String toString (String delim) {
+            if (head == Integer.MIN_VALUE) {
+                return "null";
+            }
+            String returnString = "";
+            for (int i = head; i > 0; i--) {
+                returnString += Integer.toString(stack[i]) + delim;
+            }
+            return returnString;
+        }
+    }
+
+    public class LinkedListStack {
+        public class Node {
+            Node next = null;
+            int data;
+
+            public Node(int d) {
+                data = d;
+            }
+
+            void appendToTail(int d) {
+                this.next = new Node(d);
+            }
+
+            @Override
+            public String toString() {
+                return Integer.toString(data);
+            }
+        }
+
+        private Node head = null;
+
+        LinkedListStack () {}
+
+        public LinkedListStack(int d) {
+            head = new Node(d);
+        }
+
+        public int peek() {
+            try {
+                return head.data;
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+                return Integer.MIN_VALUE;
+            }
+        }
+
+        public void append(int d) {
+            if (head == null) {
+                head = new Node(d);
+                return;
+            }
+            Node temp = new Node(d);
+            temp.next = head;
+            head = temp;
+        }
+
+        public int pop() {
+            try {
+                int returnValue = head.data;
+                head = head.next;
+                return returnValue;
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+                return Integer.MIN_VALUE;
+            }
+        }
+
+        public void clear () {
+            head = null;
+        }
+
+        public boolean empty () {
+            return head == null;
+        }
+
+        @Override
+        public String toString() {
+            Node temp = head;
+            String returnString = "";
+            if (temp == null) {
+                return "null";
+            }
+            while (temp != null) {
+                returnString += temp.data + " ";
+                temp = temp.next;
+            }
+            return returnString;
+        }
+
+        public String toString(String delim) {
+            Node temp = head;
+            String returnString = "";
+            if (temp == null) {
+                return "null";
+            }
+            while (temp != null) {
+                returnString += temp.data + delim;
+                temp = temp.next;
+            }
+            return returnString;
+        }
+    }
+}
--- CrackingTheCodingInterview(Java)/src/C3TestStack.java
+++ CrackingTheCodingInterview(Java)/src/C3TestStack.java
@@ -1,6 +1,6 @@
 public class C3TestStack {
     public static void main(String[] args) {
-        C3StackArray.Stack stack = new C3StackArray.Stack();
+        C3Stack.LinkedListStack stack = new C3Stack.LinkedListStack();
         System.out.println(stack.toString());
         stack.append(1);
         stack.append(2);
@@ -8,18 +8,11 @@ public class C3TestStack {
         stack.append(4);
         stack.append(5);
         stack.pop();
-        stack.pop();
-        stack.pop();
-        stack.pop();
+        stack.clear();
         stack.pop();
         stack.append(1);
-        stack.append(2);
         stack.append(4);
-        stack.append(8);
-        while (!stack.empty()) {
-            System.out.println(Integer.toString(stack.pop()) + " ");
-        }
-
         System.out.println(stack.toString());
+
     }
 }
