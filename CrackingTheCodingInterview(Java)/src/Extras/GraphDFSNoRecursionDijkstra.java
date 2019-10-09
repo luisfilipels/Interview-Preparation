020f265b3cc4934e95e01623fc9709cea73a61c1
Luis Filipe <luisfilipels@hotmail.com>
2019-10-09 19:41:10 -0300

    Implementado Dijkstra para matrizes de adjacências.

--- CrackingTheCodingInterview(Java)/src/Extras/GraphDFSNoRecursionDijkstra.java
+++ CrackingTheCodingInterview(Java)/src/Extras/GraphDFSNoRecursionDijkstra.java
@@ -1,9 +1,6 @@
 package Extras;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Stack;
+import java.util.*;
 
 public class GraphDFSNoRecursionDijkstra {
 
@@ -44,6 +41,46 @@ public class GraphDFSNoRecursionDijkstra {
             }
         }
 
+        Comparator<int[]> comparator = new Comparator<int[]>() {
+            @Override
+            public int compare(int[] ints, int[] t1) {
+                return ints[0] - t1[0];
+            }
+        };
+
+        ArrayList<Integer> dijkstra (int s, int e) {
+            int [] cost = new int[graph.length];
+            for (int i = 0; i < cost.length; i++) {
+                cost[i] = Integer.MAX_VALUE;
+            }
+            int [] backtrack = new int[graph.length];
+            PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(comparator);
+            cost[s] = 0;
+            priorityQueue.add(new int[]{0, s});
+            while (!priorityQueue.isEmpty()) {
+                int [] vertexWeight = priorityQueue.poll();
+                int startVertex = vertexWeight[1];
+                int dist = vertexWeight[0];
+                for (int v = 0; v < graph.length; v++) {
+                    if (graph[startVertex][v] != 0 && cost[v] > cost[startVertex] + dist) {
+                        cost[v] = cost[startVertex] + graph[startVertex][v];
+                        priorityQueue.add(new int[]{cost[v], v});
+                        backtrack[v] = startVertex;
+                    }
+                }
+            }
+            ArrayList<Integer> returnArray = new ArrayList<>();
+            int j = e;
+            returnArray.add(e);
+            while (backtrack[j] != s) {
+                returnArray.add(backtrack[j]);
+                j = backtrack[j];
+            }
+            returnArray.add(s);
+            Collections.reverse(returnArray);
+            return returnArray;
+        }
+
     }
 
     public static void main(String[] args) {
@@ -55,7 +92,8 @@ public class GraphDFSNoRecursionDijkstra {
         graph.addEdge(4, 7);
         graph.addEdge(5, 6);
         graph.addEdge(6, 7);
-        System.out.println(graph.DFS(0, 3));
+        //System.out.println(graph.DFS(0, 3));
+        System.out.println(graph.dijkstra(4, 2));
     }
 
 }
