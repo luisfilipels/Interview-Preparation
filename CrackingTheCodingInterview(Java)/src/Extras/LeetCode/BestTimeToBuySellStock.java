bdf1e5f854ea086593640873488a74eb8215c77f
Luis Filipe <luisfilipels@hotmail.com>
2019-11-29 23:47:29 -0300

    Best Time To Buy And Sell Stock

---
+++ CrackingTheCodingInterview(Java)/src/Extras/LeetCode/BestTimeToBuySellStock.java
@@ -0,0 +1,35 @@
+package Extras.LeetCode;
+
+public class BestTimeToBuySellStock {
+
+    static private int maxProfit (int [] prices) {
+        int minBuy = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minBuy) {
+                minBuy = prices[i];
+            } else if (prices[i] - minBuy > maxProfit) {
+                maxProfit = prices[i] - minBuy;
+            }
+        }
+        return maxProfit;
+    }
+
+    // Solução força bruta, quadrática
+    /*static private int maxProfit (int [] prices) {
+        int n = prices.length;
+        int max = 0;
+        for (int l = 0; l < n; l++) {
+            for (int c = l; c < n; c++) {
+                max = Math.max(max, prices[c] - prices[l]);
+            }
+        }
+        return max;
+    }*/
+
+    public static void main(String[] args) {
+        int [] prices = new int[] {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+
+}
