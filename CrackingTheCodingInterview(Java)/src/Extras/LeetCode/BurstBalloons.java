a149b3c50d8cdd0215c665b348f2951ad505b64d
Luis Filipe <luisfilipels@hotmail.com>
2019-12-02 22:30:13 -0300

    Burst Balloons

--- CrackingTheCodingInterview(Java)/src/Extras/LeetCode/BurstBalloons.java
+++ CrackingTheCodingInterview(Java)/src/Extras/LeetCode/BurstBalloons.java
@@ -4,6 +4,39 @@ import java.util.HashMap;
 
 public class BurstBalloons {
 
+    // Solução de Tushar Roy. https://www.youtube.com/watch?v=IFNibRVgFBo&t=1104s
+    static private int maxCoins (int [] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int dp[][] = new int[nums.length][nums.length];
+
+        for (int len  = 1; len <= nums.length; len++) {
+            for (int i = 0; i <= nums.length - len; i++) {
+                int j = i + len -1;
+                for (int k = i; k <= j; k++) {
+                    int left = 1;
+                    int right = 1;
+                    if (i != 0) left = nums[i-1];
+                    if (j != nums.length-1) right = nums[j+1];
+
+                    int before = 0;
+                    int after = 0;
+                    if (i != k) {
+                        before = dp[i][k-1];
+                    }
+                    if (j != k) {
+                        after = dp[k+1][j];
+                    }
+                    dp[i][j] = Math.max(left * nums[k] * right + before + after, dp[i][j]);
+                }
+            }
+        }
+        return dp[0][nums.length-1];
+    }
+
+    // Minha solução. "Correta", mas demora MUITO e não é aceita. Força bruta.
+    /*
     static int max = 0;
 
     static int [] burst (int [] nums, int i) {
@@ -38,26 +71,14 @@ public class BurstBalloons {
 
     static private void helper (int [] nums, int currentVal) {
         for (int i = 0; i < nums.length; i++) {
-            Object[] currentDp = new Object[] {nums, i};
-            int winByCurrent;
-            int current;
-            int [] newArray;
-            if (!dp.containsKey(currentDp)) {
-                newArray = burst(nums, i);
-                dp.put(currentDp, new Object[] {newArray, valAtIndex(nums, i)});
-                winByCurrent = (Integer) dp.get(currentDp)[1];
-                current = currentVal + winByCurrent;
-                max = Math.max(max, current);
-            } else {
-                newArray = (int[])dp.get(currentDp)[0];
-                winByCurrent = (int)dp.get(currentDp)[1];
-                current = currentVal + winByCurrent;
-                max = Math.max(max, current);
-            }
-
+            int current = valAtIndex(nums, i) + currentVal;
+            int [] newArray = burst(nums, i);
+            max = Math.max(max, current);
             helper(newArray, current);
         }
     }
+    */
+
 
     public static void main(String[] args) {
         int [] ints = new int[] {3, 1, 5, 8};
