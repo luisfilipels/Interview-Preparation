6bf78af7d017956f3c308bd7d2b376abc3a55555
Luis Filipe <luisfilipels@hotmail.com>
2020-03-24 00:21:10 -0300

    New solutions for Coin Change and Partition Equal Subset Sum

--- CrackingTheCodingInterview(Java)/src/Extras/LeetCode/CoinChange.java
+++ CrackingTheCodingInterview(Java)/src/Extras/LeetCode/CoinChange.java
@@ -4,9 +4,62 @@ import java.util.Arrays;
 
 public class CoinChange {
 
+    // New DP solution. Bottom-up. Beats 97.95% of the other solutions, time-wise :D
+    /*
+    As this is a bottom-up solution, we consider smaller sub-problems. We create a dp array and consider each position i
+    of it a representing the least amount of coins needed to reach i. Logically, to reach a value of 0, we need 0 coins.
+    So, for all other positions of the array we consider what would happen if we tried to insert into our solution each
+    of the coins we have. So, for the case in which we have coins = [1, 2, 5], and amount = 7:
+
+    dp = [0, x, y, z, w, h, k, l]
+
+    How can we calculate x (amount = 1)? If we add the first coin to our solution (1), we have 0 remaining, so, we return
+    dp[0] + 1 (from the coin we added to our change). We will still consider the other 2 coins, but since 1 - 2 and 1 - 5
+    are negative, we don't consider those.
+
+    dp = [0, 1, y, z, w, h, k, l]
+
+    Now, how can we calculate y (amount = 2)? We can consider adding coin (1), which will be the solution we had for
+    amount = 1, plus 1. However, we can also consider adding coin (2), which will be the solution we had for amount = 0,
+    plus 1, which will be 0 + 1 = 1. So, for amount = 2, we only need to select 1 coin.
+
+    dp = [0, 1, 1, z, w, h, k, l]
+
+    And for amount = 3? Well, we can add coin (1), in which case we will have an amount of 2 waiting to be filled. Or,
+    we can add coin (2), in which case we will have 1 waiting to be filled. In both cases, we will have used the amount
+    of coins used in amount = 2 or amount = 1, plus one, so, 2 coins.
+
+    dp = [0, 1, 1, 2, w, h, k, l]
+
+    Finally, for amount = 4. We can try using coin (1), in which case we wil have used the amount of coins used for
+    amount = 3, plus 1, so, 3 coins. Or, we can use coin (2), which will result in us using the amount of coins used for
+    amount = 2, plus 1, so, 2 coins. So, we end up using coin (2), and so on...
+
+    dp = [0, 1, 1, 2, 2, h, k, l]
+
+     */
+    static int coinChange(int[] coins, int amount) {
+        int [] dp = new int[amount+1];
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            int min = amount+1;
+            for (int c = 0; c < coins.length; c++) {
+                int complement = i - coins[c];
+                if (complement < 0) {
+                    continue;
+                }
+                min = Math.min(dp[complement] + 1, min);
+            }
+            dp[i] = min;
+        }
+        return dp[amount] == amount+1 ? -1 : dp[amount];
+    }
+
+    // Old DP solution. Top-down. Don't fully understand it.
+    /*
     private static int coinChange (int [] coins, int amount) {
         Arrays.sort(coins);
-        dp = new int[amount];
+        dp = new int[amount+1];
         int result = helper(coins, amount);
         if (result == Integer.MAX_VALUE) {
             return -1;
@@ -20,8 +73,8 @@ public class CoinChange {
     private static int helper (int [] coins, int amountRemaining) {
         if (amountRemaining < 0) return -1;
         if (amountRemaining == 0) return 0;
-        if (dp[amountRemaining - 1] != 0) {
-            return dp[amountRemaining - 1];
+        if (dp[amountRemaining] != 0) {
+            return dp[amountRemaining];
         }
         int min = Integer.MAX_VALUE;
         for (int i = coins.length-1; i >= 0; i--) {
@@ -30,13 +83,14 @@ public class CoinChange {
                 min = currentCheck + 1;
             }
         }
-        dp[amountRemaining - 1] = min == Integer.MAX_VALUE ? -1 : min;
-        return dp[amountRemaining - 1];
+        dp[amountRemaining] = min == Integer.MAX_VALUE ? -1 : min;
+        return dp[amountRemaining];
     }
 
     public static void main(String[] args) {
         int [] coins = new int[] {1, 2, 5};
-        System.out.println(coinChange(coins, 4));
+        System.out.println(coinChange(coins, 11));
     }
+    */
 
 }
--- CrackingTheCodingInterview(Java)/src/Extras/LeetCode/PartitionEqualSubsetSum.java
+++ CrackingTheCodingInterview(Java)/src/Extras/LeetCode/PartitionEqualSubsetSum.java
@@ -4,8 +4,104 @@ import java.util.HashMap;
 
 public class PartitionEqualSubsetSum {
 
+    // New DP solution (fully mine, and faster :D)
+    /*
+    Before delving into the solution itself, notice that the summation of all the elements in our nums array MUST be even. If it's odd, we would have one partition summing up to
+    an odd number and the other, to an even number, and that goes against what's asked.
+    If we were to solve this question in a brute force manner, we could consider that we have two empty partitions. Let's call them left and right. Consider the following example:
+    [1 5 11 5]
+    For each element in the array, we can either add it to left partition, or to the right partition. So let's do so, for each element, using the following notation:
+    x -> Sum in left partition
+    y -> Sum in right partition
+    Going up, we add the number at index idx into the left partition. Going down, we do the same, but to the right partition.
+    Let's consider inserting first into the left partition.
+
+idx: 0 1    2               3
+
+              ______________22
+             |              0
+             |
+            17              17
+        ___  0______________5
+       |
+       6     6              11 <---
+     _ 0 __ 11 _____________11 <--- Solution
+    |        |
+ __\ 1       |______________6
+   / 0                      16
+    |_
+       1 __ 12_______________17
+       5     5               5
+       |     |__________
+       |___ 1  ___...   |___ 12
+            16               10
+             |____...
+
+     Notice that if we do the same considering the first insertion into the right partition, we will essentially have a mirror of what we see above.
+     Also notice that that, for any idx we may be at, we take into the next iteration the value that's currently in the left partition, and what's in the right partition, and our index.
+     Thus, our brute force code is quite simple:
+
+     public boolean canPartition(int[] nums) {
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        if (total % 2 != 0) {
+            return false;
+        } else {
+            return helper(nums, 0, 0, 0);
+        }
+    }
+
+     boolean helper (int [] nums, int currentPos, int left, int right) {
+        if (currentPos == nums.length) { // If we added all numbers into the partitions...
+            return left == right;        // we return true if both partitions sum up to the same value. Else, false.
+        }
+        // Else, we consider if either summing the current value to the left partition or to the right partition are true
+        return helper(nums, currentPos + 1, left + nums[currentPos], right) || helper(nums, currentPos + 1, left, right + nums[currentPos]);
+    }
+
+    How could we improve the above solution though?
+    First, consider that "int total" must be even, and that total/2 == total/2. With this, we could get rid of taking with us one of the partitions into the next recursive call.
+    Why? Because, if we take total/2 into the recursion, then whenever currentSum == total/2, that means that we reached the solution. Also, notice that, instead of considering
+    that we take the values of two partitions into the recursion, we could instead consider that we either take the current number into the next call, or not. We could "pretend"
+    that by not taking the current value in the array, we are actually inserting it into the other partition.
+    Next, notice that, with the above, the only variables we are taking into recursive calls are the index we are currently at (currentPos) and the value of the sum of all
+    elements in the partition we are summing into (currentSum). With this, we can memoize these values into a HashMap, with the key being (currentPos + " " + currentSum), and the
+    value being a boolean indicating if with this combination of currentPos and currentSum a solution is reachable or not. Thus, we arrive at the following code:
+     */
+
+    static boolean canPartition(int[] nums) {
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        if (total % 2 != 0) {   // If the total sum of the array is odd, there's no way we could partition it in the way the question asks to.
+            return false;
+        } else {
+            return helper(nums, 0, 0, total/2, new HashMap<>());
+        }
+    }
+
+    static boolean helper (int [] nums, int currentPos, int currentSum, int totalHalf, HashMap<String, Boolean> hash) {
+        String current = currentPos + " " + currentSum;
+        if (hash.containsKey(current)) {
+            return hash.get(current);
+        }
+        if (currentSum == totalHalf) {
+            hash.put(current, true);
+            return true;
+        }
+        if (currentSum > totalHalf || currentPos == nums.length) {  // If the currentSum is greater than totalHalf, further adding values into currentSum will be useless, since there are only positive integers.
+            hash.put(current, false);
+            return false;
+        }
+        hash.put(current, helper(nums, currentPos + 1, currentSum + nums[currentPos], totalHalf, hash) || helper(nums, currentPos + 1, currentSum, totalHalf, hash));
+        return hash.get(current);
+    }
+
     // First DP solution. Passes, but is slow and uses too much space.
-    enum cases{
+    /*enum cases{
         UNKNOWN, TRUE, FALSE
     }
 
@@ -43,7 +139,7 @@ public class PartitionEqualSubsetSum {
         boolean dontTake = helper(nums, pos + 1, currentSum, total, dp);
         dp[currentSum][pos] = (take || dontTake) ? cases.TRUE : cases.FALSE;
         return take || dontTake;
-    }
+    }*/
 
     /*
     // Alternative solution. TLE for some reason.
