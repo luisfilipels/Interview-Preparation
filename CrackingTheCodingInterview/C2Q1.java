be802f37883dda2885aa56ea50ba34dd98d254db
Luis Filipe <luisfilipels@hotmail.com>
2020-07-06 21:12:59 -0300

    Reviewing questions from Chapter 2 of CtCI

--- CrackingTheCodingInterview/C2Q1.java
+++ CrackingTheCodingInterview/C2Q1.java
@@ -2,12 +2,42 @@ import java.util.HashSet;
 
 public class C2Q1 {
 
-    public static void removeDups(C2LinkedList.LinkedList list) {
-        C2LinkedList.Node current = list.head;
+    static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+
+        @Override
+        public String toString() {
+            Node temp = this;
+            StringBuilder sb = new StringBuilder();
+            while (temp != null) {
+                sb.append(temp.val).append(" ");
+                temp = temp.next;
+            }
+            return sb.toString();
+        }
+
+        void append(int val) {
+            Node temp = this;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(val);
+        }
+    }
+
+    // O(n²) time solution, O(1) space.
+    // For each node in the list, check for nodes with the same value in the nodes that come after the node head.
+    public static void removeDups(Node head) {
+        Node current = head;
         while (current.next != null) {
-            C2LinkedList.Node runner = current;
+            Node runner = current;
             while (runner.next != null) {
-                if (runner.next.data == current.data) {
+                if (runner.next.val == current.val) {
                     runner.next = runner.next.next;
                 } else {
                     runner = runner.next;
@@ -17,22 +47,39 @@ public class C2Q1 {
         }
     }
 
+    // O(n) time solution, O(n) space.
+    // Go through the whole LL, inserting the value of each node into a hashset in case the value is not already in the
+    // hashset. If it is in the hashset, point previous.next to temp.next, effectively removing the current (duplicate)
+    // node from the LL.
+    public static void removeDupsHash(Node head) {
+        HashSet<Integer> hash = new HashSet<>();
+        Node temp = head;
+        Node prev = null;
+        while (temp != null) {
+            if (hash.contains(temp.val)) {
+                prev.next = temp.next;
+            } else {
+                hash.add(temp.val);
+                prev = temp;
+            }
+            temp = temp.next;
+        }
+    }
+
 
     public static void main(String[] args) {
 
-        C2LinkedList.LinkedList list = new C2LinkedList.LinkedList();
-        list.append(3);
-        list.append(2);
-        list.append(6);
-        list.append(2);
-        list.append(6);
-        list.append(2);
-        list.append(1);
-        list.append(2);
-        list.append(2);
-        removeDups(list);
-        System.out.println(list.toString());
-
+        Node head = new Node(3);
+        head.append(2);
+        head.append(2);
+        head.append(2);
+        head.append(2);
+        head.append(2);
+        head.append(1);
+        head.append(2);
+        head.append(2);
+        removeDupsHash(head);
+        System.out.println(head.toString());
 
     }
 
--- CrackingTheCodingInterview/C2Q2.java
+++ CrackingTheCodingInterview/C2Q2.java
@@ -1,36 +1,81 @@
 public class C2Q2 {
 
-    public static C2LinkedList.Node kThToLast (C2LinkedList.LinkedList list, int k) {
-        C2LinkedList.Node forward = list.head;
-        C2LinkedList.Node backward = list.head;
-        int i = 0;
-        while (forward.next != null) {
-            forward = forward.next;
-            i++;
-            if (i > k) {
-                backward = backward.next;
-            }
+    static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
         }
-        return backward;
+
+        @Override
+        public String toString() {
+            Node temp = this;
+            StringBuilder sb = new StringBuilder();
+            while (temp != null) {
+                sb.append(temp.val).append(" ");
+                temp = temp.next;
+            }
+            return sb.toString();
+        }
+
+        void append(int val) {
+            Node temp = this;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(val);
+        }
     }
 
-    /*
-    // Solução trivial, trapaça, feia, não-estilosa, blablabla. Não utilizar '--
-    public static C2LinkedList.Node kThToLast (C2LinkedList.LinkedList list, int k) {
-        C2LinkedList.Node temp = list.head;
-        int size = list.size();
-        int j = 0;
-        while (temp.next != null && j < size - k) {
+    static Node LLfromArray(int[] arr) {
+        Node head = new Node(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            head.append(arr[i]);
+        }
+        return head;
+    }
+
+    // Approach 1, using two pointers. O(n) time, O(1) space.
+    // Use two pointers, last and first. Move first forward k times. After this, move last and first forward at the same
+    // time, until first is null. When this is done, last will be the node that is the Kth to last.
+    static Node kthToLast (Node head, int k) {
+        Node last = head;
+        Node first = head;
+
+        for (int i = 0; i < k; i++) {
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            last = last.next;
+        }
+
+        return last;
+    }
+
+    // Approach 2, counting the number of nodes. O(n) time, O(1) space.
+    // First, count the number of elements in the LL. Logically, if there are n elements in the LL, the kTh to last element
+    // will be the n - k element of the LL, counting from the head node.
+    static Node kthToLast2 (Node head, int k) {
+        int n = 0;
+        Node temp = head;
+        while (temp != null) {
             temp = temp.next;
-            j++;
+            n++;
+        }
+        temp = head;
+        for (int i = 0; i < n - k; i++) {
+            temp = temp.next;
         }
         return temp;
-    }*/
+    }
 
     public static void main(String[] args) {
         int [] input = {1, 2, 3, 4, 5};
-        C2LinkedList.LinkedList list = new C2LinkedList.LinkedList(input);
-        System.out.println(kThToLast(list, 1).data);
+        Node head = LLfromArray(input);
+        System.out.println(kthToLast2(head, 1).val);
 
     }
 }
