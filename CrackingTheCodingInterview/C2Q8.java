139b0c060b4b4cf5a1100860d4d6f86f9000160b
Luis Filipe <luisfilipels@hotmail.com>
2020-07-14 21:59:58 -0300

    Reviewed 2 questions from CtCI.

--- CrackingTheCodingInterview/C2Q8.java
+++ CrackingTheCodingInterview/C2Q8.java
@@ -1,30 +1,81 @@
+import java.util.HashSet;
+
 public class C2Q8 {
 
-    // Trivial implementation with a hash table.
+    static class Node {
+        int val;
+        Node next;
 
-    public static C2LinkedList.Node findLoop (C2LinkedList.LinkedList list) {
-        C2LinkedList.Node slow = list.head;
-        C2LinkedList.Node fast = list.head;
-        while (fast != null && fast.next != null) {
+        Node(int val) {
+            this.val = val;
+        }
+
+        void append(int val) {
+            Node temp = this;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(val);
+        }
+
+        void append(Node node) {
+            Node temp = this;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+
+    static Node LLfromArray(int[] arr) {
+        Node head = new Node(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            head.append(arr[i]);
+        }
+        return head;
+    }
+
+    // Trivial implementation is with a hash table.
+    static Node findLoopHash (Node head) {
+        HashSet<Node> hashSet = new HashSet<>();
+        Node temp = head;
+        while (temp != null) {
+            if (!hashSet.contains(temp)) {
+                hashSet.add(temp);
+            } else {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    // Optimal solution is with FLoyd's Algorithm
+    static Node findLoop (Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+
             if (slow == fast) {
-                slow = list.head;
+                fast = head;
                 while (slow != fast) {
                     slow = slow.next;
                     fast = fast.next;
                 }
-                return slow;
+                return fast;
             }
         }
         return null;
     }
 
     public static void main(String[] args) {
-        C2LinkedList.LinkedList list = new C2LinkedList.LinkedList(new int[] {1, 2, 3, 4});
-        //list.head.next.next.next.next = list.head.next;
+        Node head = LLfromArray(new int[] {1, 2, 3, 4, 5, 6});
+        head.append(head.next.next.next);
         try {
-            System.out.println(findLoop(list).data);
+            System.out.println(findLoop(head).val);
         } catch (NullPointerException e) {
             System.out.println("null");
         }
--- CrackingTheCodingInterview/C3Q1.java
+++ CrackingTheCodingInterview/C3Q1.java
@@ -1,3 +1,6 @@
+import java.security.InvalidParameterException;
+import java.util.EmptyStackException;
+
 public class C3Q1 {
 
     /*
@@ -19,5 +22,126 @@ public class C3Q1 {
         TODO Implement one of the above!
 
      */
+
+    static class ThreeStacks {
+        int [] array;
+
+        int[] arrStr = new int[3];
+        int[] arrEnd = new int[3];
+        int[] arrPtr = new int[3];
+
+        int[] size = new int[3];
+
+        ThreeStacks() {
+            int individualSize = 2;
+
+            array = new int[individualSize * 3];
+            size[0] = individualSize;
+            size[1] = individualSize;
+            size[2] = individualSize;
+
+            arrStr[0] = 0;
+            arrEnd[0] = individualSize-1;
+            arrPtr[0] = 0;
+
+            arrStr[1] = individualSize;
+            arrEnd[1] = (individualSize * 2)-1;
+            arrPtr[1] = arrStr[1];
+
+            arrStr[2] = individualSize * 2;
+            arrEnd[2] = (individualSize * 3)-1;
+            arrPtr[2] = arrStr[2];
+        }
+
+        void push(int stack, int x) {
+            if (stack < 0 || stack >= 3) return;
+
+            if (arrPtr[stack] == arrEnd[stack]) {
+                int currentSize = array.length;
+                int sizeIncrement = size[stack];
+                size[stack] *= 2;
+                int [] newArr = new int[currentSize+sizeIncrement];
+
+                switch (stack) {
+                    case 0:
+                        for (int i = arrStr[1]; i <= arrEnd[1]; i++) {
+                            newArr[i + sizeIncrement] = array[i];
+                        }
+                        for (int i = arrStr[2]; i <= arrEnd[2]; i++) {
+                            newArr[i + sizeIncrement] = array[i];
+                        }
+                        for (int i = arrStr[0]; i <= arrEnd[0]; i++) {
+                            newArr[i] = array[i];
+                        }
+
+                        arrEnd[0] += sizeIncrement;
+                        arrStr[1] += sizeIncrement;
+                        arrEnd[1] += sizeIncrement;
+                        arrStr[2] += sizeIncrement;
+                        arrEnd[2] += sizeIncrement;
+
+                        arrPtr[1] += sizeIncrement;
+                        arrPtr[2] += sizeIncrement;
+                        break;
+                    case 1:
+                        for (int i = arrStr[2]; i <= arrEnd[2]; i++) {
+                            newArr[i + sizeIncrement] = array[i];
+                        }
+                        for (int i = arrStr[1]; i <= arrEnd[1]; i++) {
+                            newArr[i] = array[i];
+                        }
+                        for (int i = arrStr[0]; i <= arrEnd[0]; i++) {
+                            newArr[i] = array[i];
+                        }
+
+                        arrEnd[1] += sizeIncrement;
+                        arrStr[2] += sizeIncrement;
+                        arrEnd[2] += sizeIncrement;
+
+                        arrPtr[2] += sizeIncrement;
+                        break;
+                    case 2:
+                        for (int i = 0; i < array.length; i++) {
+                            newArr[i] = array[i];
+                        }
+                        arrEnd[2] += sizeIncrement;
+                }
+                array = newArr;
+            }
+
+            array[arrPtr[stack]] = x;
+            arrPtr[stack]++;
+        }
+
+        int pop(int stack) {
+            if (stack < 0 || stack >= 3) throw new InvalidParameterException();
+
+            if (arrPtr[stack] == arrStr[stack]) throw new EmptyStackException();
+
+            arrPtr[stack]--;
+
+            return array[arrPtr[stack]];
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreeStacks stacks = new ThreeStacks();
+        stacks.push(0, 1);
+        stacks.push(0, 2);
+        stacks.push(0, 3);
+        stacks.push(1, 10);
+        stacks.push(2, 10);
+        stacks.push(1, 11);
+        stacks.push(2, 12);
+        stacks.push(0, 4);
+        stacks.push(1, 12);
+        stacks.push(1, 13);
+        System.out.println(stacks.pop(0));
+        System.out.println(stacks.pop(0));
+        System.out.println(stacks.pop(2));
+        System.out.println(stacks.pop(0));
+        System.out.println(stacks.pop(0));
+        System.out.println(stacks.pop(0));
+    }
 
 }
