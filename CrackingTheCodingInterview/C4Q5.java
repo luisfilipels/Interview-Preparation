2c00ed4224b698a8c0a1a27ec28a092fb8dd0323
Luis Filipe <luisfilipels@hotmail.com>
2020-07-31 18:01:57 -0300

    Reviewed 2 questions from CtCI.

--- CrackingTheCodingInterview/C4Q5.java
+++ CrackingTheCodingInterview/C4Q5.java
@@ -3,82 +3,46 @@ import java.util.List;
 
 public class C4Q5 {
 
-    private static void inOrderTraversal(C4Tree.TreeNode node, List<C4Tree.TreeNode> list) {
-        if (node != null) {
-            inOrderTraversal(node.right, list);
-            list.add(node);
-            inOrderTraversal(node.left, list);
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode (int x) {
+            val = x;
         }
     }
 
-    static boolean isValidBst (C4Tree.TreeNode root) {
-        return isValidBst(root, null, null);
+    static boolean isValidBst (TreeNode root) {
+        return helper(root, null, null);
     }
 
-    static boolean isValidBst(C4Tree.TreeNode node, Integer min, Integer max) {
-        if (node == null) {
-            return true;
-        }
-        if ((min != null && node.value <= min) || (max != null && node.value > max)) {      // If the current value is less than the previously computed minimum... Conversely for the max.
-            return false;
-        }
-        return isValidBst(node.left, min, node.value) && isValidBst(node.right, node.value, max);
+    static boolean helper (TreeNode root, Integer min, Integer max) {
+        if (root == null) return true;
 
+        if (min != null) {
+            if (root.val < min) return false;
+        }
+
+        if (max != null) {
+            if (root.val > max) return false;
+        }
+
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
     }
-
-    /*
-    The following solution doesn't take into account children with the same value as their parent.
-
-    private static boolean isValidBst (C4Tree.TreeNode root) {
-        ArrayList<C4Tree.TreeNode> list = new ArrayList<>();
-
-        inOrderTraversal(root, list);
-
-        if (list.size() <= 1) {
-            return true;
-        }
-
-        int current, previous;
-
-        current = list.get(1).value;
-        previous = list.get(0).value;
-
-        for (int i = 1; i < list.size(); i++) {
-            current = list.get(i).value;
-            if (current > previous) {
-                return false;
-            } else {
-                previous = current;
-            }
-        }
-        return true;
-    }
-    */
 
     public static void main(String[] args) {
-        C4Tree.TreeNode root = new C4Tree.TreeNode(8);
-        root.left = new C4Tree.TreeNode(3);
-        root.left.left = new C4Tree.TreeNode(1);
-        root.left.right = new C4Tree.TreeNode(6);
-        root.right = new C4Tree.TreeNode(10);
-        root.right.right = new C4Tree.TreeNode(14);
-        root.right.right.left = new C4Tree.TreeNode(13);
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(6);
+        root.left.right.left = new TreeNode(4);
+        root.left.right.right = new TreeNode(9);
+        root.right = new TreeNode(10);
+        root.right.right = new TreeNode(14);
+        root.right.right.left = new TreeNode(13);
 
         System.out.println(isValidBst(root));
-        //System.out.println(isValidBst(root));
-
-        /*ArrayList preOrder = new ArrayList();
-        preOrderTraversal(root, preOrder);
-        System.out.println(preOrder);
-
-        ArrayList inOrder = new ArrayList();
-        inOrderTraversal(root, inOrder);
-        System.out.println(inOrder);
-
-        ArrayList postOrder = new ArrayList();
-        postOrderTraversal(root, postOrder);
-        System.out.println(postOrder);
-        */
 
     }
 
--- CrackingTheCodingInterview/C4Q6.java
+++ CrackingTheCodingInterview/C4Q6.java
@@ -1,47 +1,83 @@
+import java.util.HashMap;
+
 public class C4Q6 {
 
-    static class TreeNodeWithLinkToParent {
+    /*
+    For a better view of this question, let's build a BST in which all elements to the right of a certain node are greater
+    than it, and all elements to the left will be less than it.
 
-        int value;
-        TreeNodeWithLinkToParent parent;
-        TreeNodeWithLinkToParent left;
-        TreeNodeWithLinkToParent right;
+                                                    8
+                                                 /     \
+                                                3       10
+                                              /   \        \
+                                             1     6       14
+                                                 /   \     /
+                                                4     7   13
 
-        TreeNodeWithLinkToParent (TreeNodeWithLinkToParent parent, int value) {
-            this.parent = parent;
-            this.value = value;
+    Remember that in an in-order traversal, for each node, we traverse the left subtree, then we visit the node, and finally
+    traverse the right subtree. Doing so for the tree above we have: 1, 3, 4, 6, 7, 8, 10, 13, 14. Whenever we have a properly
+    formed BST, running an in-order traversal on it will result in all of the values of said BST to be presented in sorted order.
+    As such, notice that due to the way we form BSTs, given a node that has a right subtree, any successor it may have will be
+    on said subtree. But what if it does not have a right subtree?
+
+     */
+
+    // HashMap used only for testing.
+    static HashMap<Integer, TreeNode> hash = new HashMap<>();
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode parent;
+
+        TreeNode(int val) {
+            this.val = val;
+            hash.put(val, this);
+        }
+
+        void createLeft (int val) {
+            left = new TreeNode(val);
+            left.parent = this;
+            hash.put(val, left);
+        }
+
+        void createRight (int val) {
+            right = new TreeNode(val);
+            right.parent = this;
+            hash.put(val, right);
         }
     }
 
-    static TreeNodeWithLinkToParent successor (TreeNodeWithLinkToParent node) {
-        if (node.right == null) {
-            TreeNodeWithLinkToParent temp = node;
-            TreeNodeWithLinkToParent upUntilRight = temp.parent;
-            while (upUntilRight != null && upUntilRight.left != temp) {
-                temp = upUntilRight;
-                upUntilRight = upUntilRight.parent;
+    static TreeNode findSuccessor (TreeNode node) {
+        TreeNode temp;
+        if (node.right != null) {
+            temp = node.right;
+            while (temp.left != null) {
+                temp = temp.left;
             }
-            return upUntilRight;
-        }
-        TreeNodeWithLinkToParent temp = node.right;
-        while (temp.left != null) {
-            temp = temp.left;
+        } else {
+            temp = node;
+            while (temp.val <= node.val) {
+                temp = temp.parent;
+                if (temp == null) return null;
+            }
         }
         return temp;
-
     }
 
     public static void main(String[] args) {
-        TreeNodeWithLinkToParent root = new TreeNodeWithLinkToParent(null, 8);
-        root.left = new TreeNodeWithLinkToParent(root, 3);
-        root.left.left = new TreeNodeWithLinkToParent(root.left, 1);
-        root.left.right = new TreeNodeWithLinkToParent(root.left, 6);
-        root.right = new TreeNodeWithLinkToParent(root, 10);
-        root.right.right = new TreeNodeWithLinkToParent(root.right, 14);
-        root.right.right.left = new TreeNodeWithLinkToParent(root.right.right, 13);
-        root.left.right.left = new TreeNodeWithLinkToParent(root.left.right, 4);
+        TreeNode root = new TreeNode(8);
+        root.createLeft(3);
+        root.createRight(10);
+        root.left.createLeft(1);
+        root.left.createRight(6);
+        root.left.right.createLeft(4);
+        root.left.right.createRight(7);
+        root.right.createRight(14);
+        root.right.right.createLeft(13);
 
-        System.out.println(successor(root.left.right).value);
+        System.out.println(findSuccessor(hash.get(11)).val);
 
     }
 
