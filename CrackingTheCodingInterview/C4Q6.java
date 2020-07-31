86223bf6818596d2c8365d0dc9629b355cee6c80
Luis Filipe <luisfilipels@hotmail.com>
2020-07-31 18:44:45 -0300

    Finished explanation on C4Q6 (Successor)

--- CrackingTheCodingInterview/C4Q6.java
+++ CrackingTheCodingInterview/C4Q6.java
@@ -17,9 +17,24 @@ public class C4Q6 {
     Remember that in an in-order traversal, for each node, we traverse the left subtree, then we visit the node, and finally
     traverse the right subtree. Doing so for the tree above we have: 1, 3, 4, 6, 7, 8, 10, 13, 14. Whenever we have a properly
     formed BST, running an in-order traversal on it will result in all of the values of said BST to be presented in sorted order.
-    As such, notice that due to the way we form BSTs, given a node that has a right subtree, any successor it may have will be
-    on said subtree. But what if it does not have a right subtree?
 
+    As such, notice that due to the way we form BSTs, given a node that has a right subtree, any successor it may have MUST be
+    on said subtree. So how do find this element? An equivalent question may be, given a node with value X, and its right subtree
+    (that has all of its elements with values greater than X), how do we find the minimum value of this subtree? Consider the
+    node with value 3 in the tree above. Its successor must be in its right subtree, with root 6. If we go to the right from
+    there, we will only find nodes with value greater than 6, which is not what we want. However, if we go to the left subtree,
+    we will only find values that greater than 3, and less than 6. If so, if we continue going to the left, starting from 6,
+    we reach 4 (that is the successor of 3). We can repeat this process for any node that has a right subtree to find its successor.
+
+    But what if it does not have a right subtree? That may either mean we've reached the largest number of the entire BST,
+    or that we've finished traversing the left subtree of a certain node and that said node is the successor of the node
+    we're currently trying to find its successor. In either case, given we have parent pointers, we can simply go up on the
+    tree until we find a node whose value is greater than the current node's value (remember, if we are in the left subtree
+    of a node, then all the values in this subtree must have values that are less than that of the root node), meaning that
+    we've found the root node of the subtree.
+
+    In a balanced BST, this algorithm has an O(h) time complexity, with h being its height, and an O(1) space complexity.
+    Its time complexity is O(n) in an unbalanced tree, with n being the amount of nodes in the tree.
      */
 
     // HashMap used only for testing.
