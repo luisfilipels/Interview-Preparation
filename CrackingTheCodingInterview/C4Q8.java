db2f80396f011ed87c4008ae535ad6e4bb82567f
Luis Filipe <luisfilipels@hotmail.com>
2020-08-04 22:24:37 -0300

    Reviewed 2 questions from CtCI.

--- CrackingTheCodingInterview/C4Q8.java
+++ CrackingTheCodingInterview/C4Q8.java
@@ -2,7 +2,7 @@ public class C4Q8 {
 
     // Thanks to the explanation of https://www.youtube.com/channel/UCZLJf_R2sWyUtXSKiKlyvAw
 
-    static C4Tree.TreeNode commonAncestor (C4Tree.TreeNode root, C4Tree.TreeNode p, C4Tree.TreeNode q) {
+    /**static C4Tree.TreeNode commonAncestor (C4Tree.TreeNode root, C4Tree.TreeNode p, C4Tree.TreeNode q) {
         if (root == null) {
             return null;
         }
@@ -24,18 +24,79 @@ public class C4Q8 {
         } else {
             return right;
         }
+    }*/
+
+    static class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+
+        TreeNode (int val) {
+            this.val = val;
+        }
+
+        void createLeft (int val) {
+            left = new TreeNode(val);
+        }
+
+        void createRight (int val) {
+            right = new TreeNode(val);
+        }
+    }
+
+    // We use these booleans to check whether both nodes have been found or not.
+    // Useful when we can't find a common ancestor.
+    static boolean foundP = false;
+    static boolean foundQ = false;
+
+
+    static TreeNode commonAncestorHelper (TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p) {
+            foundP = true;
+            return root;
+        } else if (root == q) {
+            foundQ = true;
+            return root;
+        }
+
+        if (root == null) return null;
+
+        TreeNode left = commonAncestorHelper(root.left, p, q);
+        TreeNode right = commonAncestorHelper(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        } else {
+            if (left != null) return left;
+            else if (right != null) return right;
+            else return null;
+        }
+    }
+
+    static TreeNode commonAncestor (TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+
+        TreeNode result = commonAncestorHelper(root, p, q);
+
+        if (foundQ && foundP) return result;
+        else return null;
     }
 
     public static void main(String[] args) {
-        C4Tree.BinaryCompleteTree tree = new C4Tree.BinaryCompleteTree();
+        TreeNode main = new TreeNode(1);
+        main.createRight(3);
+        main.right.createLeft(6);
+        main.right.createRight(7);
+        main.right.left.createRight(11);
+        TreeNode root = new TreeNode(2);
+        main.left = root;
+        root.createLeft(4);
+        root.createRight(5);
+        root.left.createLeft(8);
+        root.left.createRight(9);
+        root.right.createRight(10);
 
-        for (int i = 1; i < 15; i++) {
-            tree.add(i);
-        }
-
-        System.out.println(commonAncestor(tree.root, tree.root.left.left.left, tree.root).value);
-
-
+        System.out.println(commonAncestor(main, root.left.left, root.right.right).val);
 
     }
 }
--- CrackingTheCodingInterview/C4Q9.java
+++ CrackingTheCodingInterview/C4Q9.java
@@ -1,49 +1,79 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class C4Q9 {
 
-    static ArrayList<LinkedList<Integer>> findListOfDepths (C4Tree.TreeNode root) {
-        ArrayList<LinkedList<Integer>> returnList = new ArrayList<>();
-        if (root != null) {
-            findListOfDepths(root, returnList, 0);
+    static class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+
+        TreeNode (int val) {
+            this.val = val;
+        }
+
+        void createLeft (int val) {
+            left = new TreeNode(val);
+        }
+
+        void createRight (int val) {
+            right = new TreeNode(val);
+        }
+    }
+
+    static ArrayList<String> bstSequences (TreeNode root) {
+        HashSet<String> hash = new HashSet<>();
+        helper(root, "", new ArrayList<>(), hash);
+        int maxSize = 0;
+        for (String s: hash) {
+            maxSize = Math.max(maxSize, s.length());
+        }
+        ArrayList<String> toRemove = new ArrayList<>();
+        for (String s: hash) {
+            if (s.length() != maxSize) {
+                toRemove.add(s);
+            }
+        }
+        for (String r : toRemove) {
+            hash.remove(r);
+        }
+        ArrayList<String> returnList = new ArrayList<>();
+        for (String s : hash) {
+            returnList.add(s);
         }
         return returnList;
     }
 
-    static void findListOfDepths (C4Tree.TreeNode node, ArrayList<LinkedList<Integer>> returnList, int nodeDepth) {
-        if (node != null) {
-            try {
-                returnList.get(nodeDepth).add(node.value);
-            } catch (Exception e) {
-                returnList.add(new LinkedList<>());
-                returnList.get(nodeDepth).add(node.value);
-            }
-            findListOfDepths(node.left, returnList, nodeDepth + 1);
-            findListOfDepths(node.right, returnList, nodeDepth + 1);
+    static void helper (TreeNode current, String currentString, ArrayList<TreeNode> options, HashSet<String> hash) {
+        if (current == null) return;
+        currentString += " " + current.val;
+        options.remove(current);
+        if (current.left != null) {
+            options.add(current.left);
         }
+        if (current.right != null) {
+            options.add(current.right);
+        }
+        for (TreeNode t : options) {
+            helper(t, currentString, new ArrayList<>(options), hash);
+        }
+        hash.add(currentString);
     }
 
-    //static ArrayList<String> bstSequences (C4Tree.TreeNode root) {
-
-    // TODO Implement this question
-
-    //}
-
     public static void main(String[] args) {
-        C4Tree.BinarySearchTree bst = new C4Tree.BinarySearchTree();
+        TreeNode root = new TreeNode(4);
+        root.createLeft(2);
+        root.createRight(5);
+        root.left.createLeft(1);
+        root.left.createRight(3);
+        root.right.createRight(6);
 
-        bst.add(5);
-
-        bst.add(2);
-        bst.add(7);
-
-        bst.add(1);
-        bst.add(3);
-
-        bst.add(8);
-
-        System.out.println(findListOfDepths(bst.root));
+        ArrayList<String> result = bstSequences(root);
+        for (String s : result) {
+            System.out.println(s);
+        }
     }
 
 }
