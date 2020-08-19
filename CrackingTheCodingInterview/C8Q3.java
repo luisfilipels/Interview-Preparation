91b1d7bbec2f039e2d9833d41186a405527236b1
Luis Filipe <luisfilipels@hotmail.com>
2020-08-18 21:21:18 -0300

    Solved 1 question from CtCI (Incomplete)

--- CrackingTheCodingInterview/C8Q3.java
+++ CrackingTheCodingInterview/C8Q3.java
@@ -1,40 +1,31 @@
 public class C8Q3 {
 
-    static boolean magicIndexNonDistinct (int [] array, int start, int end) {
-        int mid = (start + end) / 2;
-        if (array[mid] == mid) {
-            return true;
+    // With distinct integers
+    static boolean findMagicIndex (int [] array) {
+        int left = 0, right = array.length-1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid == array[mid]) return true;
+            if (mid > array[mid]) {
+                left = mid+1;
+            } else {
+                right = mid-1;
+            }
         }
-        if (start >= end) {
-            return false;
-        }
-        if (array[mid] > mid) {
-            // Se array[mid] > mid, sabemos que um número mágico pode ainda estar logo depois do número atual (pois pode
-            // estar repetido). Lógica semelhante para o else.
-            return magicIndexNonDistinct(array, start, Math.min(mid - 1, array[mid]));
-        } else {
-            return magicIndexNonDistinct(array, Math.max(mid + 1, array[mid]), end);
-        }
+        return false;
     }
 
-    static boolean magicIndexDistinct (int [] array, int start, int end) {
-        int mid = (start + end) / 2;
-        if (array[mid] == mid) {
-            return true;
-        }
-        if (start >= end) {
-            return false;
-        }
-        if (array[mid] > mid) {
-            return magicIndexDistinct(array, start, mid - 1);
-        } else {
-            return magicIndexDistinct(array, mid, end - 1);
-        }
+    // With non-distinct integers
+    static boolean findMagicIndexFollowUp (int [] array) {
+
     }
 
     public static void main(String[] args) {
-        int [] array = {4, 5, 6, 7, 8, 9, 10};
-        System.out.println(magicIndexDistinct(array, 0, array.length-1));
+        //int [] array = {4, 5, 6, 7, 8, 9, 10};
+        int [] array = new int[] {-5, -4, -3, -1, 1, 2, 7, 8, 9, 10, 11};
+        //int [] array = new int[] {-3, -2, 2};
+        //int [] array = new int[] {0, 3, 4};
+        System.out.println(findMagicIndex(array));
     }
 
 }
