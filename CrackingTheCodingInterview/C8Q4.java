6d2812b7d58381e0b1101087313bd71dcc7754ff
Luis Filipe <luisfilipels@hotmail.com>
2020-08-20 23:29:05 -0300

    Added a new solution to C8Q4

--- CrackingTheCodingInterview/C8Q4.java
+++ CrackingTheCodingInterview/C8Q4.java
@@ -1,12 +1,8 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class C8Q4 {
 
     // Done using the explicit enumeration technique.
-
     static ArrayList<Integer> getBinRep (ArrayList<Integer> arr, int rep) {
         ArrayList<Integer> returnArray = new ArrayList<>();
         for (int i = arr.size()-1; i >= 0; i--) {
@@ -25,10 +21,27 @@ public class C8Q4 {
         return powerSet;
     }
 
+    // Done using a recursive function
+    static ArrayList<ArrayList<Integer>> powerSetRecursive (ArrayList<Integer> set) {
+        HashSet<ArrayList<Integer>> powerSet = new HashSet<>();
+        powerSetRecursiveHelper(set, 0, new ArrayList<>(), powerSet);
+        return new ArrayList<>(powerSet);
+    }
+
+    static void powerSetRecursiveHelper (ArrayList<Integer> set, int currentIndex, ArrayList<Integer> currentSet, HashSet<ArrayList<Integer>> hash) {
+        hash.add(currentSet);
+        if (currentIndex >= set.size()) return;
+        ArrayList<Integer> newSet = new ArrayList<>(currentSet);
+        newSet.add(set.get(currentIndex));
+        powerSetRecursiveHelper(set, currentIndex+1, currentSet, hash);
+        powerSetRecursiveHelper(set, currentIndex+1, newSet, hash);
+    }
+
     public static void main(String[] args) {
         Integer [] array = {1, 2, 3, 4};
         ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(array));
         System.out.println(powerSet(arr));
+        System.out.println(powerSetRecursive(arr));
     }
 
 }
