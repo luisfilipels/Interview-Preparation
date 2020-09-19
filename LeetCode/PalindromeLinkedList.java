f59673e94f0dba02d76e21b7da0b526f30a7329c
Luis Filipe <luisfilipels@hotmail.com>
2020-09-19 19:43:37 -0300

    Re-solved a question from LeetCode

--- LeetCode/PalindromeLinkedList.java
+++ LeetCode/PalindromeLinkedList.java
@@ -1,5 +1,7 @@
 package Extras.LeetCode;
 
+import java.util.Stack;
+
 public class PalindromeLinkedList {
 
     static private class ListNode {
@@ -8,8 +10,60 @@ public class PalindromeLinkedList {
         ListNode (int x) { val = x; }
     }
 
-    // O(n) time and O(1) space. (Reversing the list)
+    // O(n) time and O(n) space. Using a stack
+    /*public boolean isPalindrome(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode temp = head;
+        while(temp != null) {
+            stack.push(temp);
+            temp = temp.next;
+        }
+
+        temp = head;
+        while(!stack.isEmpty()) {
+            if (stack.pop().val != temp.val) return false;
+            temp = temp.next;
+        }
+        return true;
+    }*/
+
+    private static ListNode reverse (ListNode head) {
+        ListNode previous = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = previous;
+            previous = head;
+            head = next;
+        }
+        return previous;
+    }
+
+    // O(n) time and O(1) space solution. (Reversing the list, clean)
     private static boolean isPalindrome (ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        slow = reverse(slow);
+        while (slow != null) {
+            if (head.val != slow.val) return false;
+            head = head.next;
+            slow = slow.next;
+        }
+
+        return true;
+    }
+
+    // O(n) time and O(1) space. (Reversing the list, not a clean solution)
+    /*private static boolean isPalindrome (ListNode head) {
         int size = 0;
         ListNode right = head;
         while (right != null) {
@@ -52,8 +106,7 @@ public class PalindromeLinkedList {
         }
         return true;
     }
-
-    //[1,3,2,4,3,2,1]
+    */
 
     public static void main(String[] args) {
         /*ListNode head = new ListNode(1);
@@ -65,8 +118,8 @@ public class PalindromeLinkedList {
         head.next.next.next.next.next.next = new ListNode(1);*/
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
-        head.next.next = new ListNode(2);
-        head.next.next.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(3);
         head.next.next.next.next = new ListNode(1);
         System.out.println(isPalindrome(head));
     }
