4e40c0c09eb2d6e82ce96276d3614fe30ff7f956
Luis Filipe <luisfilipels@hotmail.com>
2020-10-07 21:09:28 -0300

    Reviewed 1 question from LeetCode

--- LeetCode/MergeSortedLists.java
+++ LeetCode/MergeSortedLists.java
@@ -7,6 +7,8 @@ public class MergeSortedLists {
         ListNode next;
         ListNode (int x) { val = x; }
 
+        ListNode() {}
+
         @Override
         public String toString() {
             ListNode temp = this;
@@ -41,53 +43,10 @@ public class MergeSortedLists {
         return head.next;
     }
 
-    /*
-    // Solução esperada, porém lenta e com bastante espaço extra
-    static private ListNode mergeTwoLists (ListNode l1, ListNode l2) {
-        ListNode returnList = new ListNode(-1);
-        ListNode returnListTail = returnList;
-        ListNode i = l1;
-        ListNode j = l2;
-        while (i != null && j != null) {
-            if (i.val == j.val) {
-                returnListTail.next = new ListNode(i.val);
-                returnListTail.next.next = new ListNode(i.val);
-                returnListTail = returnListTail.next.next;
-                i = i.next;
-                j = j.next;
-            } else if (i.val < j.val) {
-                while (i.val < j.val) {
-                    returnListTail.next = new ListNode(i.val);
-                    returnListTail = returnListTail.next;
-                    i = i.next;
-                    if (i == null) break;
-                }
-            } else {
-                while (j.val < i.val) {
-                    returnListTail.next = new ListNode(j.val);
-                    returnListTail = returnListTail.next;
-                    j = j.next;
-                    if (j == null) break;
-                }
-            }
-        }
-        while (i != null) {
-            returnListTail.next = new ListNode(i.val);
-            returnListTail = returnListTail.next;
-            i = i.next;
-        }
-        while (j != null) {
-            returnListTail.next = new ListNode(j.val);
-            returnListTail = returnListTail.next;
-            j = j.next;
-        }
-        return returnList.next;
-    }*/
-
     public static void main(String[] args) {
         /*ListNode l1 = new ListNode(1);
         l1.next = new ListNode(2);
-        l1.next.next = new ListNode(4);
+        l1.next.next = new ListNode(z4);
         ListNode l2 = new ListNode(1);
         l2.next = new ListNode(3);
         l2.next.next = new ListNode(4);*/
