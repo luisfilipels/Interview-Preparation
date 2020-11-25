08b4af6393a7a7632c2009c41060a34649da890c
Luis Filipe <luisfilipels@hotmail.com>
2020-11-25 19:02:27 -0300

    Improved Basic Calculator II

--- LeetCode/BasicCalculatorII.java
+++ LeetCode/BasicCalculatorII.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 
 public class BasicCalculatorII {
 
-    private static int[] getNumber (String s, int start) {
+    /*private static int[] getNumber (String s, int start) {
         int j = start;
         StringBuilder sb = new StringBuilder();
         while (j < s.length() && Character.isDigit(s.charAt(j))) {
@@ -13,7 +13,7 @@ public class BasicCalculatorII {
             j++;
         }
         return new int[] {j, Integer.parseInt(sb.toString())};
-    }
+    }*/
 
     static int compute (int num1, int num2, char oper) {
         switch (oper) {
@@ -29,63 +29,54 @@ public class BasicCalculatorII {
         return 0;
     }
 
-    private static int calculate (String s) {
-        Deque<Integer> numStack = new ArrayDeque<>();
-        Deque<Character> opStack= new ArrayDeque<>();
+    private static int calculate(String s) {
+        Stack<Integer> numStack = new Stack<>();
+        Stack<Character> opStack = new Stack<>();
 
-        s = s.replaceAll("\\s+", "");
+        boolean isMultDiv = false;
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') continue;
             if (Character.isDigit(s.charAt(i))) {
-                int[] result = getNumber(s, i);
-                i = result[0]-1;
-                numStack.addLast(result[1]);
+                int total = 0;
+                int exp = 1;
+                while(i >= 0 && Character.isDigit(s.charAt(i))) {
+                    total += (s.charAt(i) - '0') * exp;
+                    exp *= 10;
+                    i--;
+                }
+                i++;
+                numStack.add(total);
             } else {
                 if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-                    opStack.addLast(s.charAt(i));
-                } else {
-                    Deque<Integer> numQueue = new ArrayDeque<>();
-                    Deque<Character>opQueue = new ArrayDeque<>();
+                    if (isMultDiv) {
+                        isMultDiv = false;
+                        while (!opStack.isEmpty() && (opStack.peek() == '*' || opStack.peek() == '/')) {
+                            int num1 = numStack.pop();
+                            int num2 = numStack.pop();
+                            char op = opStack.pop();
 
-                    numQueue.add(numStack.pollLast());
-                    int j = i;
-                    for (j = i; j < s.length(); j++) {
-                        if (Character.isDigit(s.charAt(j))) {
-                            int[] result = getNumber(s, j);
-                            j = result[0]-1;
-                            numQueue.add(result[1]);
-                        } else {
-                            if (s.charAt(j) == '+' || s.charAt(j) == '-') {
-                                opStack.add(s.charAt(j));
-                                break;
-                            } else {
-                                opQueue.add(s.charAt(j));
-                            }
+                            int result = compute(num1, num2, op);
+
+                            numStack.push(result);
                         }
                     }
-                    while(!opQueue.isEmpty()) {
-                        int num1 = numQueue.poll();
-                        int num2 = numQueue.poll();
-                        char op = opQueue.poll();
-
-                        int result = compute(num1, num2, op);
-
-                        numQueue.addFirst(result);
-                    }
-                    numStack.add(numQueue.poll());
-                    i = j;
+                    opStack.push(s.charAt(i));
+                } else {
+                    opStack.push(s.charAt(i));
+                    isMultDiv = true;
                 }
             }
         }
 
-        while(!opStack.isEmpty()) {
-            int num1 = numStack.pollFirst();
-            int num2 = numStack.pollFirst();
-            char oper = opStack.pollFirst();
+        while (!opStack.isEmpty()) {
+            int num1 = numStack.pop();
+            int num2 = numStack.pop();
+            char op = opStack.pop();
 
-            int result = compute(num1, num2, oper);
+            int result = compute(num1, num2, op);
 
-            numStack.addFirst(result);
+            numStack.push(result);
         }
 
         return numStack.pop();
@@ -93,7 +84,8 @@ public class BasicCalculatorII {
 
     public static void main(String[] args) {
         //String s = "3+2*2*3+5-2/2*3";
-        String s = "1-1+1";
+        //String s = "1-1+1";
+        String s= "32 + 5 / 2";
         System.out.println(calculate(s));
     }
 
