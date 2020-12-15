a7b15bff70c19d4ca3dbe328fa1a6a02d5fe8f28
Luis Filipe <luisfilipels@hotmail.com>
2020-12-14 21:48:14 -0300

    Reviewed MaximumSubArray

--- LeetCode/MaximumSubArray.java
+++ LeetCode/MaximumSubArray.java
@@ -2,9 +2,27 @@ package Extras.LeetCode;
 
 public class MaximumSubArray {
 
+    // My new solution. O(n) time, O(1) space. No DP. Slightly faster
+    private static int maxSubArray(int[] nums) {
+        int n = nums.length;
+
+        int currentSum = 0, maxSum = -1, maxVal = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            currentSum += nums[i];
+            maxVal = Math.max(maxVal, nums[i]);
+            if (currentSum < 0) {
+                currentSum = 0;
+            } else if (currentSum > maxSum){
+                maxSum = currentSum;
+            }
+        }
+        if (maxVal < 0) return maxVal;
+        return maxSum;
+    }
+
     // New solution. Easier to understand.
     // https://www.youtube.com/watch?v=tinz1fiYv0c
-    private static int maxSubArray(int[] nums) {
+    /*private static int maxSubArray(int[] nums) {
         int [] dp = new int[nums.length];
         dp[0] = nums[0];
         int max = nums[0];
@@ -13,7 +31,7 @@ public class MaximumSubArray {
             max = Math.max(max, dp[i]);
         }
         return max;
-    }
+    }*/
 
     /*private static int maxSubArray(int[] nums) {
         int maxSoFar=nums[0], maxEndingHere=nums[0];
