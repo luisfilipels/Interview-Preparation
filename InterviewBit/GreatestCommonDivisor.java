1813fec50b14a40b71b0f599091303c2c1963050
Luis Filipe <luisfilipels@hotmail.com>
2021-02-04 22:14:56 -0300

    Reviewed one question from LeetCode and solved another from InterviewBit

---
+++ InterviewBit/GreatestCommonDivisor.java
@@ -0,0 +1,6 @@
+public class GreatestCommonDivisor {
+    public int gcd(int A, int B) {
+        if (A == 0) return B;
+        return gcd(B%A, A);
+    }
+}
--- LeetCode/FactorialTrailingZeroes.java
+++ LeetCode/FactorialTrailingZeroes.java
@@ -4,8 +4,10 @@ public class FactorialTrailingZeroes {
         int count = 0;
         int divider = 5;
         while (divider <= n) {
-            count += n / divider;
-            divider *= 5;
+            // multiples of 5 occur every 5 numbers, and multiples of 2, every 2 numbers. There are many more multiples
+            // of 2 than there are of 5, so we count the amount of 5s.
+            count += n / divider; // Count the number of 5's
+            divider *= 5;   // Next, we count the amount of 25s (as it counts as 2 5s), then of 125s...
         }
         return count;
     }
